390310b53bff3ac6ea320757f9c7b49138d4ba57
Eugene test <eupushkash@ngs.ru>
2023-10-05 17:13:18 +0300

    Deleted Test

--- src/test/java/ShopRepositoryTest.java
+++ src/test/java/ShopRepositoryTest.java
@@ -18,23 +18,7 @@ public class ShopRepositoryTest {
         Product[] actual = repo.findAll();
         Assertions.assertArrayEquals(expended, actual);
     }
-    @Test
-    public void ShopTestPNegative(){
-        ShopRepository repo = new ShopRepository();
-
-        Product product1 = new Product(55,"Кофе", 245);
-        Product product2 = new Product(123,"Тесто", 60);
-        Product product3 = new Product(13,"Носки", 150);
-
-        repo.add(product1);
-        repo.add(product2);
-        repo.add(product3);
-        repo.remove(155);
-        Product[] expended ={product1,product2,product3};
-        Product[] actual = repo.findAll();
-        Assertions.assertArrayEquals(expended, actual);
-    }
-    @Test
+        @Test
     public void shouldFindExceptions(){
         ShopRepository repo = new ShopRepository();
 
